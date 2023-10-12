f5dde872f19535c24d404a50845609f92d2f69ff
emiliano <emilianordx@gmail.com>
2023-10-12 20:27:36 -0300

    new config

---
+++ lua/plugins/test.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "thenbe/neotest-playwright",
+  },
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      adapters = {
+        ["thenbe/neotest-playwright"] = {
+          persist_project_selection = true,
+          enable_dynamic_test_discovery = true,
+        },
+      },
+      status = { virtual_text = true },
+      output = { open_on_run = true },
+      quickfix = {
+        open = function()
+          if require("lazyvim.util").has("trouble.nvim") then
+            vim.cmd("Trouble quickfix")
+          else
+            vim.cmd("copen")
+          end
+        end,
+      },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      defaults = {
+        ["<leader>t"] = { name = "+test" },
+      },
+    },
+  },
+}
